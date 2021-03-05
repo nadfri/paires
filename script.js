58d51f83a103fb376c86c48c5edd7cbb74a89ff9
Nader Frigui <nadfri@gmail.com>
2021-03-05 17:40:41 +0100

    nouveau SW - version mdn

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
-const images = document.getElementsByTagName('img');
+const images = document.getElementsByTagName("img");
 const tab = [
-    "img/1.jpg", "img/1.jpg",
-    "img/2.jpg", "img/2.jpg",
-    "img/3.jpg", "img/3.jpg",
-    "img/4.jpg", "img/4.jpg",
-    "img/5.jpg", "img/5.jpg",
-    "img/6.jpg", "img/6.jpg",
-    "img/7.jpg", "img/7.jpg",
-    "img/8.jpg", "img/8.jpg",
-    "img/9.jpg", "img/9.jpg",
-    "img/10.jpg", "img/10.jpg",
-    "img/11.jpg", "img/11.jpg",
-    "img/12.jpg", "img/12.jpg",
-    "img/13.jpg", "img/13.jpg",
-    "img/14.jpg", "img/14.jpg"
+  "img/1.jpg",
+  "img/1.jpg",
+  "img/2.jpg",
+  "img/2.jpg",
+  "img/3.jpg",
+  "img/3.jpg",
+  "img/4.jpg",
+  "img/4.jpg",
+  "img/5.jpg",
+  "img/5.jpg",
+  "img/6.jpg",
+  "img/6.jpg",
+  "img/7.jpg",
+  "img/7.jpg",
+  "img/8.jpg",
+  "img/8.jpg",
+  "img/9.jpg",
+  "img/9.jpg",
+  "img/10.jpg",
+  "img/10.jpg",
+  "img/11.jpg",
+  "img/11.jpg",
+  "img/12.jpg",
+  "img/12.jpg",
+  "img/13.jpg",
+  "img/13.jpg",
+  "img/14.jpg",
+  "img/14.jpg",
 ];
 
-
-document.body.oncontextmenu = (e) => e.preventDefault();//disable rigth click
+document.body.oncontextmenu = (e) => e.preventDefault(); //disable rigth click
 
 randomize(tab);
 for (let i = 0; i < tab.length; i++) images[i].src = tab[i];
@@ -30,84 +43,85 @@ let tour = 1;
 let count = 0;
 
 for (const image of images) {
-    image.onclick = () => {
+  image.onclick = () => {
+    setTimeout(() => image.classList.remove("hidden"), 200);
+    image.classList.add("rotate");
 
-        setTimeout(() => image.classList.remove("hidden"), 200);
-        image.classList.add("rotate");
+    if (tour == 1) {
+      image.classList.add("no-pointer");
+      span.classList.remove("slide");
+      image1 = image;
+      tour++;
+    } else {
+      count++;
+      span.textContent = count;
+      span.classList.add("slide");
 
-        if (tour == 1) {
-            image.classList.add("no-pointer");
-            span.classList.remove("slide");
-            image1 = image;
-            tour++;
+      image.classList.add("no-pointer");
+      image2 = image;
+
+      if (image1.src == image2.src) {
+        image1.parentElement.classList.add("discover");
+        image2.parentElement.classList.add("discover");
+        const filtre = [...images].filter(
+          (image) => !image.parentElement.classList.contains("discover")
+        );
+
+        if (filtre.length == 0)
+          setTimeout(() => (reload.style.display = "block"), 1000);
+      } else {
+        for (const image of images) {
+          image.classList.add("no-pointer");
+          image1.parentElement.classList.add("wrong");
+          image2.parentElement.classList.add("wrong");
+
+          if (!image.parentElement.classList.contains("discover")) {
+            setTimeout(() => {
+              image.classList.add("hidden");
+              image.classList.remove("rotate");
+              image.classList.remove("no-pointer");
+              image1.parentElement.classList.remove("wrong");
+              image2.parentElement.classList.remove("wrong");
+            }, 1500);
+          }
         }
+      }
 
-        else {
-            count++;
-            span.textContent = count;
-            span.classList.add("slide");
-
-
-            image.classList.add("no-pointer");
-            image2 = image;
-
-            if (image1.src == image2.src) {
-                image1.parentElement.classList.add("discover");
-                image2.parentElement.classList.add("discover");
-                const filtre = [...images].filter(image =>
-                    !image.parentElement.classList.contains("discover"));
-
-                if (filtre.length == 0)
-                    setTimeout(() => reload.style.display = "block", 1000);
-            }
-
-            else {
-                for (const image of images) {
-                    image.classList.add("no-pointer");
-                    image1.parentElement.classList.add("wrong");
-                    image2.parentElement.classList.add("wrong");
-
-                    if (!image.parentElement.classList.contains('discover')) {
-                        setTimeout(() => {
-                            image.classList.add("hidden");
-                            image.classList.remove("rotate");
-                            image.classList.remove("no-pointer");
-                            image1.parentElement.classList.remove("wrong");
-                            image2.parentElement.classList.remove("wrong");
-                        }, 1500);
-                    }
-                }
-            }
-
-            tour = 1;
-        }
-    };
+      tour = 1;
+    }
+  };
 }
 
-
 function randomize(tab) {
-    var i, j, tmp;
-    for (i = tab.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        tmp = tab[i];
-        tab[i] = tab[j];
-        tab[j] = tmp;
-    }
-    return tab;
+  var i, j, tmp;
+  for (i = tab.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    tmp = tab[i];
+    tab[i] = tab[j];
+    tab[j] = tmp;
+  }
+  return tab;
 }
 
 //*************Service Worker ******************/
 //Register service worker to control making site work offline
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-        .register('/paires/sw.js', { scope: '/paires/' })
-        .then(function () { console.log('Service Worker for paires Registered'); });
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/paires/sw.js", { scope: "/paires/" })
+    .then(function () {
+      console.log("Service Worker for paires Registered");
+    });
 }
 
 /************Permettre le 100vh sur mobile */
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+document.documentElement.style.setProperty("--vh", `${vh}px`);
 
-const metas = document.getElementsByTagName('meta');
-metas[1].content = 'width=device-width, height=' + window.innerHeight + ' initial-scale=1.0, maximum-scale=5.0,user-scalable=0';
+const metas = document.getElementsByTagName("meta");
+metas[1].content =
+  "width=device-width, height=" +
+  window.innerHeight +
+  " initial-scale=1.0, maximum-scale=5.0,user-scalable=0";
+
+
--- sw.js
+++ sw.js
@@ -1,9 +1,90 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox-sw.js');
+// console.log(`WorkBox of Couvre-Feu V2 loaded🎉`);
 
-if (workbox) {
-  console.log(`WorkBox of paires loaded🎉`);
-  workbox.routing.registerRoute(
-    new RegExp('/*'), //cached all files
-    new workbox.strategies.StaleWhileRevalidate()
-  );
-} else {console.log(`Boo! Workbox didn't load 😬`);}
+// const { registerRoute } = workbox.routing;
+// const { StaleWhileRevalidate, CacheFirst, NetworkFirst } = workbox.strategies;
+// //const { CacheableResponsePlugin } = workbox.cacheableResponse;
+
+// // registerRoute(
+// //   ({ request }) => request.destination === 'document' ||
+// //     request.destination === 'script',
+// //   new StaleWhileRevalidate({ cacheName: 'files-cache' })
+// // );
+
+// // registerRoute(
+// //   ({ request }) => request.destination === 'style',
+// //   new StaleWhileRevalidate({ cacheName: 'css-cache' })
+// // );
+
+// registerRoute(
+//   ({ request }) => request.destination === 'script' ||
+//     request.destination === 'style',
+//   new StaleWhileRevalidate({ cacheName: 'files-cache' })
+// );
+
+// registerRoute(
+//   ({ request }) => request.destination === 'image',
+//   new CacheFirst({ cacheName: 'image-cache' }) // Use a custom cache name.
+// );
+
+// // Cache Google Fonts with a stale-while-revalidate strategy, with
+// // a maximum number of entries.
+// // registerRoute(
+// //   ({url}) => url.origin === 'https://fonts.googleapis.com' ||
+// //              url.origin === 'https://fonts.gstatic.com',
+// //   new StaleWhileRevalidate({
+// //     cacheName: 'google-fonts',
+// //     plugins: [
+// //       new ExpirationPlugin({maxEntries: 20}),
+// //     ],
+// //   }),
+// // );
+
+/******* */
+const cacheName = "files-cache-v1"; //change le nom pour mettre à jour le cache
+
+const contentToCache = ["/"];
+
+// Installing Service Worker
+self.addEventListener("install", (e) => {
+	console.log("Service Worker Installation");
+	e.waitUntil(
+		caches.open("files-cache-v1").then((cache) => {
+			console.log("Service Worker-Mise en cache globale");
+			return cache.addAll(contentToCache);
+		})
+	);
+});
+
+// Fetching content using Service Worker
+self.addEventListener('fetch', (e) => {
+	e.respondWith(
+	  caches.match(e.request).then((r) => {
+			// console.log('Service Worker-Récupération de la ressource: '+e.request.url);
+		return r || fetch(e.request).then((response) => {
+				  return caches.open(cacheName).then((cache) => {
+			// console.log('Service Worker-Mise en cache de la nouvelle ressource: '+e.request.url);
+			cache.put(e.request, response.clone());
+			return response;
+		  });
+		});
+	  })
+	);
+  });
+
+//Efface l'ancien cache non utilisé
+self.addEventListener("activate", (e) => {
+	e.waitUntil(
+		caches.keys().then((keyList) => {
+			return Promise.all(
+				keyList.map((key) => {
+					if (cacheName.indexOf(key) === -1) {
+						return caches.delete(key);
+					}
+				})
+			);
+		})
+	);
+});
+
+
